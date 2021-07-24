43315ccda06b38a66be6a0453929d481592504f4
natikos <natikos2000@gmail.com>
2021-07-24 20:09:35 +0300

    #8: scrap title

--- server/scrapping-manager.js
+++ server/scrapping-manager.js
@@ -2,25 +2,36 @@ const cheerio = require('cheerio');
 
 class ScrappingManager {
   #$root = null;
+  #$body = null;
 
   loadPage(webPage) {
     const document = cheerio.load(webPage);
-    this.#$root = document.root().html();
+    this.#$root = document.root();
+    this.#$body = document;
   }
 
   getScrappedData() {
+    if (!this.#$body || !this.#$root) {
+      throw new Error('Document was not loaded');
+    }
+
     return {
       htmlVersion: this.#htmlVersion,
+      title: this.#title,
     };
+  }
+
+  get #title() {
+    return this.#$body('title').text();
   }
 
   get #htmlVersion() {
     const startStrOfDoctype = '<!doctype';
     const endStrOfDoctype = '<html';
-    const caseInsensitiveDoc = this.#$root.toLowerCase();
+    const caseInsensitiveDoc = this.#$root.html().toLowerCase();
     const startIndex = caseInsensitiveDoc.indexOf(startStrOfDoctype);
     const endIndex = caseInsensitiveDoc.indexOf(endStrOfDoctype);
-    const doctypeStr = this.#$root.substring(startIndex, endIndex + 1);
+    const doctypeStr = this.#$root.html().substring(startIndex, endIndex + 1);
     return this.#determineHtmlVersion(doctypeStr);
   }
 
