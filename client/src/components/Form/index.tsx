c4b7bb2fbd898a5880662e619a42d5d96852565a
natikos <natikos2000@gmail.com>
2021-07-25 17:10:07 +0300

    #9: validate url form-field

--- client/src/components/Form/index.tsx
+++ client/src/components/Form/index.tsx
@@ -7,15 +7,31 @@ export default function Form() {
   const [fields, setFields] = useState({
     url: {
       value: '',
-      validate: (value: string): boolean => {
-        return false;
-      }
+      isValid: false,
     }
   });
 
   const submitUrlForAnalysis: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
     console.log('Submit');
+  };
+
+  const onUrlFieldChange = (url: string): void => {
+    setFields({
+      url: {
+        isValid: isValidUrl(url),
+        value: url
+      }
+    });
+  };
+
+  const isValidUrl = (url: string): boolean => {
+    try {
+      const urlObj = new URL(url);
+      return !!urlObj.hostname && !!urlObj.protocol;
+    } catch (_) {
+      return false;
+    }
   };
 
   return <form onSubmit={submitUrlForAnalysis}>
@@ -25,8 +41,9 @@ export default function Form() {
       placeholder='https://...'
       label='Web-site url'
       value={fields.url.value}
-      onChange={(url) => setFields({ ...fields, url: { ...fields.url, value: url } })}
+      onChange={onUrlFieldChange}
+      validation={{ message: 'Invalid url', isValid: fields.url.isValid }}
     />
-    <Button type='submit' text='Submit' />
+    <Button type='submit' text='Submit' disabled={!fields.url.isValid}/>
   </form>
 }
