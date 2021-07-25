f29226dc1559fb75bf1482fb8e875fd3fbe9742d
natikos <natikos2000@gmail.com>
2021-07-25 19:52:31 +0300

    #7: adjust message and timeout

--- server/constants.js
+++ server/constants.js
@@ -2,7 +2,7 @@ const DEFAULT_PORT = 3001;
 const HEADING_TAGS = 'h1,h2,h3,h4,h5,h6';
 const HTML5 = 'HTML 5';
 const MILISEC_IN_SEC = 1000;
-const REQUEST_TIMEOUT_IN_SEC = 60;
+const REQUEST_TIMEOUT_IN_SEC = 30;
 
 module.exports = {
   DEFAULT_PORT,
