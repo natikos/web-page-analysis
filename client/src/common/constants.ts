617c4568aabd5fc276fc73224501d6bb31157c55
natikos <natikos2000@gmail.com>
2021-07-30 18:32:52 +0300

    #26: add default server url

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -1,2 +1,3 @@
+export const DEFAULT_API_URL = 'http://localhost:3001';
 export const MAX_NOTIFICATIONS = 2;
 export const AUTOCLOSE_IN_MILISEC = 5000;
--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { IScrappedData } from 'common/interfaces';
+import { DEFAULT_API_URL } from 'common/constants';
 
 const service = {
   getScrappedInfo: async (url: string): Promise<IScrappedData> => {
-    const serverURL = process.env.REACT_APP_API_URL;
+    const serverURL = process.env.REACT_APP_API_URL ?? DEFAULT_API_URL;
     const endpoint = serverURL ? `${serverURL}/info` : '';
     const response: IScrappedData = await axios.get(
       endpoint,
